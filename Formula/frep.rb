dd759d42bd7f46de3c1d9bc323c5bd54b9c4ee58
Guoqiang Chen <subchen@gmail.com>
2017-12-31 00:27:03 +0800

    Update frep.rb

--- Formula/frep.rb
+++ Formula/frep.rb
@@ -2,7 +2,7 @@ class Frep < Formula
   desc "Generate file using template from environment, arguments, json/yaml/toml config files"
   homepage "https://github.com/subchen/frep"
   url "https://github.com/subchen/frep/releases/download/v1.2.2/frep-1.2.2-darwin-amd64"
-  version "v1.2.2"
+  version "1.2.2"
   sha256 "9e6ac9253c8518cadd55dd1627e8c78b76924c84af8825d08ce5656011399aa1"
 
   bottle :unneeded
