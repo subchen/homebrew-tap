e0dee26c512ffe8dcff875de2d55b6dd9194de98
Guoqiang Chen <subchen@gmail.com>
2018-01-08 12:20:21 +0800

    Create gojson.rb

---
+++ Formula/gojson.rb
@@ -0,0 +1,21 @@
+class Gojson < Formula
+  desc "Automatically generate Go (golang) struct definitions from example JSON"
+  homepage "https://github.com/ChimeraCoder/gojson"
+  url "https://github.com/ChimeraCoder/gojson/archive/v1.0.0.tar.gz"
+  sha256 "6acc1246547cf1d28b4a6ab0b83948076ac269e91efea9729b0d958d308357e2"
+
+  depends_on "go"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    srcpath = buildpath/"src/github.com/ChimeraCoder/gojson"
+    srcpath.install buildpath.children
+
+    cd srcpath do
+      system "go", "build", "-o", "_build/gojson", "./gojson"
+      bin.install "_build/gojson" => "gojson"
+      #prefix.install_metafiles
+    end
+  end
+
+end
