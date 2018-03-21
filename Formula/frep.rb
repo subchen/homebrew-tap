9cde5766e6854d533356ccdeb23f45a728c41a3e
Guoqiang Chen <subchen@gmail.com>
2018-03-21 11:47:53 +0800

    Update frep.rb

--- Formula/frep.rb
+++ Formula/frep.rb
@@ -1,14 +1,14 @@
 class Frep < Formula
   desc "Generate file using template from environment, arguments, json/yaml/toml config files"
   homepage "https://github.com/subchen/frep"
-  url "https://github.com/subchen/frep/releases/download/v1.3.2/frep-1.3.2-darwin-amd64"
-  version "1.3.2"
-  sha256 "eb5a1e460f83b2692f6ad7a27e22c7303fc3c788bc4b59e201eaf7c49eb11429"
+  url "https://github.com/subchen/frep/releases/download/v1.3.3/frep-1.3.3-darwin-amd64"
+  version "1.3.3"
+  sha256 "896c89e17fda2df8cabf4cb1aa9a8036d0ad978680e14cc0bf51b5427756ee52"
 
   bottle :unneeded
 
   def install
-    bin.install "frep-1.3.2-darwin-amd64" => "frep"
+    bin.install "frep-1.3.3-darwin-amd64" => "frep"
   end
   
   def test
