327291970f3dd01682a8a561458128051a9bed29
Guoqiang Chen <subchen@gmail.com>
2019-01-17 18:05:48 +0000

    Automatic update frep to 1.3.5

--- Formula/frep.rb
+++ Formula/frep.rb
@@ -3,7 +3,7 @@ class Frep < Formula
   homepage "https://github.com/subchen/frep"
   url "https://github.com/subchen/frep/releases/download/v1.3.5/frep-1.3.5-darwin-amd64"
   version "1.3.5"
-  sha256 "ad1bd7b013fce52242b63778f51c3a856b2189da4d926a2be354acc6fbdfe17a"
+  sha256 "460841304c0a2a8341e3d714e3773fdfed89ff2fcb33573511a672fa5061c56b"
 
   bottle :unneeded
 
