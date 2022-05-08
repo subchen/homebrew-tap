74ce3755637474c020489c2778863e9586cf9b2f
Guoqiang Chen <subchen@gmail.com>
2022-05-09 00:08:36 +0800

    Update frep.rb

--- Formula/frep.rb
+++ Formula/frep.rb
@@ -5,7 +5,7 @@ class Frep < Formula
 
   if Hardware::CPU.intel?
     url "https://github.com/subchen/frep/releases/download/v1.3.13/frep-1.3.13-darwin-amd64"
-    sha256 "c0281f66d05dcbbfc93bda3149dea92ac5b6fd74a2ec8ded44a64c92bfe5dd89"
+    sha256 "8429f760609938be3c7bddde8ab7857538d1b1ee5bb0158b2d9f67dec3719dd8"
 
     def install
       bin.install "frep-1.3.13-darwin-amd64" => "frep"
@@ -13,7 +13,7 @@ class Frep < Formula
   end
   if Hardware::CPU.arm?
     url "https://github.com/subchen/frep/releases/download/v1.3.13/frep-1.3.13-darwin-arm64"
-    sha256 "4d17140260e03078169e119fb7fbf24438232081d120000728c897e9dd11c607"
+    sha256 "c2b43e3670a37e09f03bfb362da3a575137f5fa1158e255f8af602fd5e187431"
 
     def install
       bin.install "frep-1.3.13-darwin-arm64" => "frep"
